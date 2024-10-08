11704bb07a732256b76858c945c11654ac5656f9
liz771124 <liz771124@yahoo.com.tw>
2024-10-08 18:43:55 +0800

    fix: 測試更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import 'tw-elements/css/tw-elements.min.css';
+import { supabase } from '@/lib/supabase'
+
 
 export default function Home() {
+  const setNewView = async ()=> {
+    const { data, error } = await supabase.from("views").insert({
+      name: 'radom name'
+    });
+    if(data) console.log(data)
+    if(data) console.log(error)
+  }
+
+  async function fetchData() {
+    const { data } = await supabase.from('views').select('*');
+    if(data) console.log(data)
+  }
+  fetchData();
+
+  setNewView()
+
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">歡迎來到我的個人網站</h1>
-      <p>這是我的個人網站首頁。請使用上方的導航欄查看更多信息。</p>
+      <div>1234</div>
     </div>
   );
 }
